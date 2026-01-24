ac5235eb9a80cd367970cc5e802619ae4ccfcb76
DUBEY-AKASH <akashdwivedi99894@gmail.com>
2026-01-25 01:39:16 +0530

    state lock

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
 terraform {
+    backend "s3" {
+      bucket = "terraformstateemo"
+      key = "ec2-demo/terrform.tfstate"
+      region = "eu-north-1"
+      dynamodb_table = "terraform-locks"
+      encrypt = true
+    }
