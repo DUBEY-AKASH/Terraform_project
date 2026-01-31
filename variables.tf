098e0d8f8c45a446202fe578692e97e4d01a0621
DUBEY-AKASH <akashdwivedi99894@gmail.com>
2026-01-31 20:37:26 +0530

    chnages

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidr" {
-    default = "10.0.0.0/24"
+    default = "10.0.1.0/24"
   
 }
 
 variable "private_subnet_cidr" {
-    default = "10.0.0.0/24"
+    default = "10.0.2.0/24"
   
 }
 
